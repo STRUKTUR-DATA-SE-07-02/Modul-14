45471be97639538ce2aeec5ba93a8b4b0ad58b53
Satriaariq23 <136548620+Satriaariq23@users.noreply.github.com>
2024-12-23 20:27:43 +0700

    Add files via upload

---
+++ 2211104033_Satria Ariq Adelard Dompas/UNGUIDED/Unguided1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+void printMatrix(const vector<vector<int>> &matrix, const vector<string> &nodes)
+{
+    int n = nodes.size();
+
+    cout << setw(8) << "";
+    for (int i = 0; i < n; i++)
+    {
+        cout << setw(8) << nodes[i];
+    }
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << setw(8) << nodes[i];
+        for (int j = 0; j < n; j++)
+        {
+            cout << setw(8) << matrix[i][j];
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int numNodes;
+    cout << "Silakhan Masukan Jumlah Simpul: ";
+    cin >> numNodes;
+
+    vector<string> nodes(numNodes);
+    cout << "Silakan Masukan Nama Simpul:\n";
+    for (int i = 0; i < numNodes; i++)
+    {
+        cout << "Simpul " << i + 1 << ": ";
+        cin >> nodes[i];
+    }
+
+    vector<vector<int>> weights(numNodes, vector<int>(numNodes, 0));
+
+    cout << "Silakan Masukkan Bobot antar Simpul:\n";
+    for (int i = 0; i < numNodes; i++)
+    {
+        for (int j = 0; j < numNodes; j++)
+        {
+            if (i != j || (i == j && weights[i][j] == 0))
+            {
+                cout << nodes[i] << " --> " << nodes[j] << " = ";
+                cin >> weights[i][j];
+            }
+        }
+    }
+
+    cout << "\n";
+    printMatrix(weights, nodes);
+
+    return 0;
+}
