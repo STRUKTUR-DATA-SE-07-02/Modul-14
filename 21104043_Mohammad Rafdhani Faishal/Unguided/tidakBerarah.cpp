7d21b363f96c35d9ca791f0fdbc0342aa04c8f22
Mohammad Rafdani Faishal <41566401+lapachie8@users.noreply.github.com>
2025-01-12 22:59:38 +0700

    Add files via upload
    first commit

---
+++ 21104043_Mohammad Rafdhani Faishal/Unguided/tidakBerarah.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, e;
+    cout << "Masukkan jumlah simpul: ";
+    cin >> n;
+
+    cout << "Masukkan jumlah sisi: ";
+    cin >> e;
+
+    vector<vector<int>> adjMatrix(n, vector<int>(n, 0));
+
+    cout << "Masukkan pasangan simpul:\n";
+    for (int i = 0; i < e; i++) {
+        int u, v;
+        cin >> u >> v;
+        adjMatrix[u - 1][v - 1] = 1;
+        adjMatrix[v - 1][u - 1] = 1; 
+    }
+
+    cout << "\nAdjacency Matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << adjMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
